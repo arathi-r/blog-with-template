0bb310f424ff69592cf87af9787b8befd0ea9c3c
arathi <arathiramesh005@gmail.com>
2019-01-30 06:03:32 +0000

    templates,css supported

--- my-first-blog-master/firstpro/app1/views.py
+++ my-first-blog-master/firstpro/app1/views.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from . models import Post
 from django.shortcuts import render
+from django.contrib.auth.models import User
+
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    me=User.objects.get(username='amma')
+    posts = Post.objects.filter(author=me).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
